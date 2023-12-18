9a5d04cf4d8fd6bce9ea883b0ba873ee777f7c33
BAYKUSEFENDI <136642793+BAYKUSEFENDI@users.noreply.github.com>
2023-12-18 18:59:12 +0100

    Update wt-test.py

--- wt-test.py
+++ wt-test.py
@@ -1,2 +1,11 @@
-import pywhatkit
-pywhatkit.sendwhatmsg_instantly("+90","testtest",1)
+import pywhatkit as kit
+import datetime
+
+phone_number = "+1234567890"  # Replace with the recipient's phone number, including the country code
+message = "Hello, World!"  # Replace with your desired message
+
+now = datetime.datetime.now()
+hours = now.hour
+minutes = now.minute + 2  # Send the message 2 minutes from now
+
+kit.sendwhatmsg(phone_number, message, hours, minutes)
