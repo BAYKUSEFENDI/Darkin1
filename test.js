b988b6308c0932d01e78e5bec8c25942b017c548
BAYKUSEFENDI <136642793+BAYKUSEFENDI@users.noreply.github.com>
2025-07-13 18:10:40 +0300

    Update test.js

--- test.js
+++ test.js
@@ -3,3 +3,10 @@ import {web3} from "./node_modules/web3-js"
 
 const contract = new ethers.contract("CONTRACTADDRESS",NETWORK,X)
 
+const contract = new ethers.contract("CONTRACTADDRESS",NETWORK,X)
+
+const contract = new ethers.contract("CONTRACTADDRESS",NETWORK,X)
+
+
+const contract = new ethers.contract("CONTRACTADDRESS",NETWORK,X)
+
